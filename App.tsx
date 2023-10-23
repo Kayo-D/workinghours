7cd350934b8700b13fa5acd9ebe9a23e97fd5dfd
Kayo-D <113675667+Kayo-D@users.noreply.github.com>
2023-10-23 02:39:29 +0200

    added navigation to a details page it does not update properly. always show current month

--- App.tsx
+++ App.tsx
@@ -2,8 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Button, TextInput, Text, ScrollView, Image } from 'react-native';
 import { Calendar } from 'react-native-calendars';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useRoute, RouteProp } from '@react-navigation/native';
 
-export default function App() {
+type RootStackParamList = {
+  Home: undefined,
+  Details: { markedDates: { [date: string]: { marked: boolean; selected: boolean; selectedColor: string; hoursWorked?: number; dotColor?: string } } };
+};
+
+type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
+
+type ProfileScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+};
+
+function HomeScreen({ navigation }: Props) {
   const [numberInput, setNumberInput] = useState<string>('');
   const [markedDates, setMarkedDates] = useState<{
     [date: string]: { marked: boolean; selected: boolean; selectedColor: string; hoursWorked?: number; dotColor?: string };
@@ -67,81 +87,99 @@ export default function App() {
 
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View>
-        <Image
-          source={require('./images/torestorpmaleri.png')}
-          style={{ width: 400, height: 200 }}
-        />
-      </View>
-      <Calendar
-        style={{
-          height: 400,
-          width: 400,
-        }}
-        theme={{
-          selectedDayTextColor: 'black',
-          todayTextColor: 'black',
-        }}
-        markedDates={markedDates}
-        firstDay={1}
-        hideExtraDays={true}
-        onDayPress={(day) => {
-          const dateStr = day.dateString;
-          if (markedDates[dateStr]) {
-            markedDates[dateStr].selected = !markedDates[dateStr].selected;
-          } else {
-            markedDates[dateStr] = {
-              marked: false,
-              selected: true,
-              selectedColor: 'lightblue',
-            };
-          }
-          setMarkedDates({ ...markedDates });
-        }}
-        // Custom rendering of calendar dates
-        renderDay={(day: { year: number; month: number; day: number }, dateInfo: { marked: boolean; selected: boolean; hoursWorked?: number; dotColor?: string }) => (
-          <View style={{ backgroundColor: getCalendarBackgroundColor(dateInfo), padding: 5 }}>
-            <Text style={{ textAlign: 'center', color: 'black' }}>{day.day}</Text>
-            {dateInfo.dotColor && <Text style={{ color: dateInfo.dotColor }}>•</Text>}
-          </View>
-        )}
-      />
-      <StatusBar style="auto" backgroundColor='white' />
-      <TextInput
-        placeholder='Hours'
-        style={styles.hourInputContainer}
-        keyboardType="numeric"
-        onChangeText={(text) => setNumberInput(text)}
-        value={numberInput}
-      />
-      <Button
-        title="Add Hours"
-        onPress={handleButtonPress}
-      />
-      <Button
-        title="See hours worked for this month"
-        onPress={handleButtonPress}
-      />
-      {Object.keys(markedDates).map((date) => {
-        const dateInfo = markedDates[date];
-        const currentDate = new Date(date);
-
-        // Check if the current date is in the same month as the current date displayed on the calendar
-        if (currentDate.getMonth() === new Date().getMonth() && currentDate.getFullYear() === new Date().getFullYear()) {
-          return (
-            <View key={date}>
-              <Text>
-                Date: {date}, Hours Worked: {dateInfo.hoursWorked || '0'}
-              </Text>
+        <View>
+          <Image
+            source={require('./images/torestorpmaleri.png')}
+            style={{ width: 400, height: 175 }}
+          />
+        </View>
+        <Calendar
+          style={{
+            height: 400,
+            width: 400,
+          }}
+          theme={{
+            selectedDayTextColor: 'black',
+            todayTextColor: 'black',
+          }}
+          markedDates={markedDates}
+          firstDay={1}
+          hideExtraDays={true}
+          onDayPress={(day) => {
+            const dateStr = day.dateString;
+            if (markedDates[dateStr]) {
+              markedDates[dateStr].selected = !markedDates[dateStr].selected;
+            } else {
+              markedDates[dateStr] = {
+                marked: false,
+                selected: true,
+                selectedColor: 'lightblue',
+              };
+            }
+            setMarkedDates({ ...markedDates });
+          }}
+          // Custom rendering of calendar dates
+          renderDay={(day: { year: number; month: number; day: number }, dateInfo: { marked: boolean; selected: boolean; hoursWorked?: number; dotColor?: string }) => (
+            <View style={{ backgroundColor: getCalendarBackgroundColor(dateInfo), padding: 5 }}>
+              <Text style={{ textAlign: 'center', color: 'black' }}>{day.day}</Text>
+              {dateInfo.dotColor && <Text style={{ color: dateInfo.dotColor }}>•</Text>}
             </View>
-          );
-        }
+          )}
+        />
+        <StatusBar style="auto" backgroundColor='white' />
+        <TextInput
+          placeholder='Hours'
+          style={styles.hourInputContainer}
+          keyboardType="numeric"
+          onChangeText={(text) => setNumberInput(text)}
+          value={numberInput}
+        />
+        <Button
+          title="Add Hours"
+          onPress={handleButtonPress}
+        />
+        <Button
+          title="See hours worked for this month"
+          onPress={() => navigation.navigate('Details', { markedDates })}
+        />
+      </ScrollView>
+  );
+}
 
-        // Return null for dates that are not in the current month
-        return null;
-      })}
+const Stack = createNativeStackNavigator();
 
-    </ScrollView>
+function DetailsScreen() {
+  const route = useRoute<DetailsScreenRouteProp>();
+  const markedDates = route.params.markedDates;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {Object.keys(markedDates).map((date) => {
+          const dateInfo = markedDates[date];
+          const currentDate = new Date(date);
+
+          if (currentDate.getMonth() === new Date().getMonth() && currentDate.getFullYear() === new Date().getFullYear()) {
+            return (
+              <View key={date}>
+                <Text>
+                  Date: {date}, Hours Worked: {dateInfo.hoursWorked || '0'}
+                </Text>
+              </View>
+            );
+          }
+          return null;
+        })}
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -164,3 +202,5 @@ const styles = StyleSheet.create({
     height: 40,
   }
 });
+
+export default App;
